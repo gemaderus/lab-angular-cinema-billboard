7c64761fe50e20f86d237669e10043e129420362
gemaderus <hi@gemaderus.com>
2017-11-29 18:14:46 +0100

    Second iteration finished.

--- starter-code/src/app/app.module.ts
+++ starter-code/src/app/app.module.ts
@@ -1,18 +1,27 @@
+import { Routes } from "@angular/router";
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
+import { routes } from './routes';
+import { MyhomeComponent } from './myhome/myhome.component';
+import { MymovieComponent } from './mymovie/mymovie.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyhomeComponent,
+    MymovieComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
